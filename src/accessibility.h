8563e080cc4f8615babfd2afcc0dac3e61f4cc9a
anvibabenko <anvibabenko@edu.hse.ru>
2025-06-19 11:13:06 +0300

    тестовый заход с trip_id

--- src/accessibility.h
+++ src/accessibility.h
@@ -56,6 +56,13 @@ class Accessibility {
     vector<vector<int>> Routes(vector<long> sources, vector<long> targets,  
                                int graphno = 0);
 
+    // shortest path with trip_ids between two points
+    vector<int> RouteWithTripIds(int src, int tgt, int graphno = 0);
+
+    // shortest path with trip_ids between list of origins and destinations
+    vector<vector<int>> RoutesWithTripIds(vector<long> sources, vector<long> targets,  
+                                          int graphno = 0);
+
     // shortest path distance between two points
     double Distance(int src, int tgt, int graphno = 0);
     
@@ -66,11 +73,20 @@ class Accessibility {
     // precompute the range queries and reuse them
     void precomputeRangeQueries(float radius);
 
+    // set trip_ids for the network
+    void set_trip_ids(vector<int> trip_ids);
+
     // aggregation types
     vector<string> aggregations;
 
     // decay types
     vector<string> decays;
+
+    // accessibility_vars_t is a vector of floating point values
+    // assigned to each node - the first level of the data structure
+    // is dereferenced by node index
+    typedef vector<vector<float> > accessibility_vars_t;
+    map<string, accessibility_vars_t> accessibilityVars;
 
  private:
     double maxdist;
@@ -82,11 +98,6 @@ class Accessibility {
     // by time of day
     vector<std::shared_ptr<Graphalg> > ga;
 
-    // accessibility_vars_t is a vector of floating point values
-    // assigned to each node - the first level of the data structure
-    // is dereferenced by node index
-    typedef vector<vector<float> > accessibility_vars_t;
-    map<string, accessibility_vars_t> accessibilityVars;
     // this is a map for pois so we can keep track of how many
     // pois there are at each node - for now all the values are
     // set to one, but I can imagine using floating point values
--- src/graphalg.cpp
+++ src/graphalg.cpp
@@ -1,4 +1,6 @@
 #include "graphalg.h"
+#include "accessibility.h"
+#include "shared.h"
 #include <math.h>
 
 namespace MTC {
@@ -7,6 +9,7 @@ Graphalg::Graphalg(
         int numnodes, vector< vector<long> > edges, vector<double> edgeweights,
         bool twoway) {
     this->numnodes = numnodes;
+    this->edges_storage = edges;
 
     int num = omp_get_max_threads();
     
@@ -44,6 +47,91 @@ Graphalg::Graphalg(
     ch.RunPreprocessing();
 }
 
+Graphalg::Graphalg(
+        int numnodes, vector< vector<long> > edges, vector<double> edgeweights,
+        vector<int> trip_ids, bool twoway) : numnodes(numnodes), trip_ids(trip_ids) {
+    this->edges_storage = edges;
+
+    int num = omp_get_max_threads();
+    
+    FILE_LOG(logINFO) << "Generating contraction hierarchies with "
+                      << num << " threads.\n";
+    
+    ch = CH::ContractionHierarchies(num);
+
+    vector<CH::Node> nv;
+
+    for (int i = 0 ; i < numnodes ; i++) {
+        // CH allows you to pass in a node id, and an x and a y, and then
+        // never uses it - to be clear, we don't pass it in anymore
+        CH::Node n(i, 0, 0);
+        nv.push_back(n);
+    }
+
+    FILE_LOG(logINFO) << "Setting CH node vector of size "
+                      << nv.size() << "\n";
+	
+    ch.SetNodeVector(nv);
+
+    vector<CH::Edge> ev;
+
+    for (int i = 0 ; i < edges.size() ; i++) {
+        int trip_id = (i < trip_ids.size()) ? trip_ids[i] : -1;
+        CH::Edge e(edges[i][0], edges[i][1], i,
+            edgeweights[i]*DISTANCEMULTFACT, true, twoway, trip_id, i);
+        ev.push_back(e);
+    }
+
+    FILE_LOG(logINFO) << "Setting CH edge vector of size "
+                      << ev.size() << "\n";
+    
+    ch.SetEdgeVector(ev);
+    ch.RunPreprocessing();
+}
+
+Graphalg::Graphalg(
+    int numnodes,
+    vector< vector<long> > edges, vector<double> edgeweights,
+    bool twoway,
+    class Accessibility* accessibility_ptr) : numnodes(numnodes), accessibility_ptr(accessibility_ptr) {
+    
+    this->edges_storage = edges;
+
+    int num = omp_get_max_threads();
+    
+    FILE_LOG(logINFO) << "Generating contraction hierarchies with "
+                      << num << " threads.\n";
+    
+    ch = CH::ContractionHierarchies(num);
+
+    vector<CH::Node> nv;
+
+    for (int i = 0 ; i < numnodes ; i++) {
+        // CH allows you to pass in a node id, and an x and a y, and then
+        // never uses it - to be clear, we don't pass it in anymore
+        CH::Node n(i, 0, 0);
+        nv.push_back(n);
+    }
+
+    FILE_LOG(logINFO) << "Setting CH node vector of size "
+                      << nv.size() << "\n";
+	
+    ch.SetNodeVector(nv);
+
+    vector<CH::Edge> ev;
+
+    for (int i = 0 ; i < edges.size() ; i++) {
+        CH::Edge e(edges[i][0], edges[i][1], i,
+            edgeweights[i]*DISTANCEMULTFACT, true, twoway, -1, i);
+        ev.push_back(e);
+    }
+
+    FILE_LOG(logINFO) << "Setting CH edge vector of size "
+                      << ev.size() << "\n";
+    
+    ch.SetEdgeVector(ev);
+    ch.RunPreprocessing();
+}
 
 std::vector<NodeID> Graphalg::Route(int src, int tgt, int threadNum) {
     std::vector<NodeID> ResultingPath;
@@ -58,6 +146,103 @@ std::vector<NodeID> Graphalg::Route(int src, int tgt, int threadNum) {
         threadNum);
 
     return ResultingPath;
+}
+
+std::vector<int> Graphalg::RouteWithTripIds(int src, int tgt, int threadNum) {
+    std::cout << "DEBUG: RouteWithTripIds called with src=" << src << ", tgt=" << tgt << ", threadNum=" << threadNum << std::endl;
+    
+    std::vector<NodeID> ResultingPath;
+    std::vector<int> ResultingTripIds;
+
+    CH::Node src_node(src, 0, 0);
+    CH::Node tgt_node(tgt, 0, 0);
+
+    ch.computeShortestPath(
+        src_node,
+        tgt_node,
+        ResultingPath,
+        threadNum);
+
+    // Debug info
+    std::cout << "DEBUG: After computeShortestPath, ResultingPath size = " << ResultingPath.size() << std::endl;
+    if (ResultingPath.size() > 0) {
+        std::cout << "DEBUG: ResultingPath = [";
+        for (size_t i = 0; i < ResultingPath.size(); ++i) {
+            std::cout << ResultingPath[i];
+            if (i + 1 < ResultingPath.size()) std::cout << ", ";
+        }
+        std::cout << "]" << std::endl;
+    }
+
+    // Get trip_ids from accessibilityVars through accessibility_ptr
+    std::vector<int> trip_ids;
+    if (accessibility_ptr != nullptr) {
+        std::cout << "DEBUG: Looking for trip_ids in accessibilityVars" << std::endl;
+        
+        // Check if trip_ids are available in accessibilityVars
+        auto trip_ids_iter = accessibility_ptr->accessibilityVars.find("trip_ids");
+        if (trip_ids_iter != accessibility_ptr->accessibilityVars.end()) {
+            const auto& trip_ids_vars = trip_ids_iter->second;
+            std::cout << "DEBUG: Found trip_ids in accessibilityVars, size = " << trip_ids_vars.size() << std::endl;
+
+            // Convert accessibilityVars to vector<int> for easier access
+            for (const auto& node_vars : trip_ids_vars) {
+                for (const auto& var : node_vars) {
+                    trip_ids.push_back(static_cast<int>(var));
+                }
+            }
+            
+            std::cout << "DEBUG: Converted trip_ids size = " << trip_ids.size() << std::endl;
+        } else {
+            std::cout << "DEBUG: trip_ids not found in accessibilityVars" << std::endl;
+        }
+    } else {
+        std::cout << "DEBUG: accessibility_ptr is null" << std::endl;
+    }
+
+    FILE_LOG(logINFO) << "RouteWithTripIds: Path size = " << ResultingPath.size() << "\n";
+    FILE_LOG(logINFO) << "RouteWithTripIds: Edges storage size = " << edges_storage.size() << "\n";
+    FILE_LOG(logINFO) << "RouteWithTripIds: Trip IDs size = " << trip_ids.size() << "\n";
+
+    // For each consecutive pair of nodes in the path, find the corresponding edge and its trip_id
+    for (size_t i = 0; i < ResultingPath.size() - 1; ++i) {
+        int from_node = ResultingPath[i];
+        int to_node = ResultingPath[i + 1];
+        
+        std::cout << "DEBUG: Looking for edge from " << from_node << " to " << to_node << std::endl;
+        
+        // Find the edge in edges_storage
+        bool found = false;
+        for (size_t j = 0; j < edges_storage.size(); ++j) {
+            if (edges_storage[j][0] == from_node && edges_storage[j][1] == to_node) {
+                int trip_id = (j < trip_ids.size()) ? trip_ids[j] : -1;
+                std::cout << "DEBUG: Found edge at index " << j << " with trip_id = " << trip_id << std::endl;
+                ResultingTripIds.push_back(trip_id);
+                found = true;
+                break;
+            }
+        }
+        
+        if (!found) {
+            std::cout << "DEBUG: No matching edge found for " << from_node << " -> " << to_node << std::endl;
+            ResultingTripIds.push_back(-1);
+        }
+    }
+
+    FILE_LOG(logINFO) << "RouteWithTripIds: Returning " << ResultingTripIds.size() << " trip IDs\n";
+    std::string trip_ids_str = "[";
+    for (size_t i = 0; i < ResultingTripIds.size(); ++i) {
+        trip_ids_str += std::to_string(ResultingTripIds[i]);
+        if (i + 1 < ResultingTripIds.size()) trip_ids_str += ", ";
+    }
+    trip_ids_str += "]";
+    FILE_LOG(logINFO) << "RouteWithTripIds: Trip ID path = " << trip_ids_str << "\n";
+    
+    // Добавляем вывод в консоль для отладки
+    std::cout << "DEBUG: RouteWithTripIds returning trip_ids: " << trip_ids_str << std::endl;
+    std::cout << "DEBUG: ResultingTripIds size: " << ResultingTripIds.size() << std::endl;
+    
+    return ResultingTripIds;
 }
 
 
